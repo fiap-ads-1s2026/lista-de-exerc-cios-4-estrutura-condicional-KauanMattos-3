12d5d4bfa65d4c25412609726c5b7b0d78325bb9
Kauan Mattos <kauanosmattos@gmail.com>
2026-04-09 20:29:56 -0300

    Exercícios Finalizados até 02

--- exercicio1.py
+++ exercicio1.py
@@ -2,9 +2,25 @@ class Exercicio1:
     """Algoritmo que receba duas notas escolares e calcule a média aritmética"""
     
     def calcular_media_notas(self):
-        print()
+        n1 = float(input("Digite sua primeira nota (0.0 a 10.0): "))
+        n2 = float(input("Digite sua segunda nota (0.0 a 10.0): "))
 
+        # Validação
+        if n1 > 10 or n2 > 10 or n1 < 0 or n2 < 0:
+            print("ERRO: As notas devem estar no intervalo de 0.0 a 10.0")
+            return # Encerra o método
+        
+        # Cálculo
+        else:
+            media = (n1 + n2) / 2
+
+        # Saída
+        print(f"Nota 1: {n1}")
+        print(f"Nota 2: {n2}")
+        print(f"Média aritmética: {media:.1f}")
+    
 
 if __name__ == "__main__":
     exercicio = Exercicio1()
     exercicio.calcular_media_notas()
+
--- exercicio2.py
+++ exercicio2.py
@@ -2,7 +2,22 @@ class Exercicio2:
     """Algoritmo que analisa a viabilidade de um crédito financeiro"""
     
     def analisar_credito(self):
-        print()
+        salario_bruto = float(input("Digite o sálario bruto (R$): "))
+        parcela = float(input("Digite o valor mensal da parcela desejada (R$): "))
+
+        if parcela <= 0.20 * salario_bruto:
+            porcentagem = (parcela / salario_bruto) * 100
+            print(f"Salário bruto (R$): {salario_bruto}")
+            print(f"Valor da parcela (R$): {parcela}")
+            print(f"Comprometimento de renda (%): {porcentagem}%")
+            print("SOLICITAÇÃO APROVADA: O valor está dentro do limite permitido")
+        else:
+            porcentagem = (parcela / salario_bruto) * 100
+            print(f"Salário bruto (R$): {salario_bruto}")
+            print(f"Valor da parcela (R$): {parcela}")
+            print(f"Comprometimento de renda (%): {porcentagem}%")
+            print(f"SOLICITAÇÃO NEGADA: A prestação excede 20% do salário")
+
 
 
 if __name__ == "__main__":
