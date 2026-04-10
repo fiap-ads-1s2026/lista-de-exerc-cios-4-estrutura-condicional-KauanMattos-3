1e4b250a2a551b4600f337b24ddb832db7c81b52
Kauan Mattos <kauanosmattos@gmail.com>
2026-04-09 21:45:31 -0300

    Exercicios feitos até 02

--- exercicio1.py
+++ exercicio1.py
@@ -2,8 +2,8 @@ class Exercicio1:
     """Algoritmo que receba duas notas escolares e calcule a média aritmética"""
     
     def calcular_media_notas(self):
-        n1 = float(input("Digite sua primeira nota (0.0 a 10.0): "))
-        n2 = float(input("Digite sua segunda nota (0.0 a 10.0): "))
+        n1 = float(input("Digite a sua primeira nota (0.0 a 10.0): "))
+        n2 = float(input("Digite a sua segunda nota (0.0 a 10.0): "))
 
         # Validação
         if n1 > 10 or n2 > 10 or n1 < 0 or n2 < 0:
@@ -15,8 +15,8 @@ class Exercicio1:
             media = (n1 + n2) / 2
 
         # Saída
-        print(f"Nota 1: {n1}")
-        print(f"Nota 2: {n2}")
+        print(f"Nota 1: {n1:.2f}")
+        print(f"Nota 2: {n2:.2f}")
         print(f"Média aritmética: {media:.1f}")
     
 
